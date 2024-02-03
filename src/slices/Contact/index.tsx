1d0cd1c19d638b56c6fd726ebfbb1bf5594dd9d3
aakash gadekar <Aakash Gadekar>
2024-02-03 15:48:21 +0530

    first commit

---
+++ src/slices/Contact/index.tsx
@@ -0,0 +1,23 @@
+import { Content } from "@prismicio/client";
+import { SliceComponentProps } from "@prismicio/react";
+
+/**
+ * Props for `Contact`.
+ */
+export type ContactProps = SliceComponentProps<Content.ContactSlice>;
+
+/**
+ * Component for "Contact" Slices.
+ */
+const Contact = ({ slice }: ContactProps): JSX.Element => {
+  return (
+    <section
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+    >
+      Placeholder component for contact (variation: {slice.variation}) Slices
+    </section>
+  );
+};
+
+export default Contact;
